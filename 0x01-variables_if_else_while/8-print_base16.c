ff06602b55cf70544254417d58825c2f0181b21a
maryamawitG <maryamawit4kidus@gmail.com>
2022-12-09 14:49:59 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	int b;
+
+	ch = 'a';
+	while
+		(b < 10)
+		{
+			putchar(b + '0');
+			b++;
+		}
+	while
+		(ch <= 'f')
+		{
+			putchar(ch);
+			ch++;
+		}
+	return (0);
+}
