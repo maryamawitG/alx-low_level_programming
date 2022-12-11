a9555e1f75b6754caa28e2dfad7e6a5044714311
maryamawitG <maryamawit4kidus@gmail.com>
2022-12-11 06:00:32 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,17 +16,17 @@ int main(void)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
-				if (a < b && b< c)
+				if (a < b && b < c)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
 					if (!(a == '7' && b == '8' && c == '9'))
-					{
+							{
 							putchar(',');
 							putchar(' ');
-					}
-				}
+							}
+				}	
 			}
 		}
 	}
