acea85a8e21bccc435228041de419969a640d68a
maryamawitG <maryamawit4kidus@gmail.com>
2022-12-13 12:52:53 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char ch;
+	int i = 0;
+
+	while (i <= 26)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar('a');
+			ch++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
