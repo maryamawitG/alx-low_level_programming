605fa1ed1a2889eff3fc406fc54f4b7ddf6f6d99
maryamawitG <maryamawit4kidus@gmail.com>
2022-12-13 15:45:52 -0800

    7. There are only 3 colors, 10 digits, and 7 no
    tes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,6 +12,13 @@ int print_last_digit(int n)
 	int i;
 
 	i = n % 10;
-	_putchar(i + 48);
-	return (i);
+		if (i < 0)
+		{
+			_putchar(i * -1);
+			return (i * -1);
+		}
+		else
+		{
+			_putchar(i + 48);
+			return (i);
 }
