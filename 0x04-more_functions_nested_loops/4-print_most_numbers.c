335b8523a666532f317a1f5c9fa984a397b9953a
maryamawitG <maryamawit4kidus@gmail.com>
2022-12-15 12:32:19 -0800

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,11 @@ void print_most_numbers(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		if (a != 50 && a != 52)
+		if ((a == 50) || (a == 52))
 		{
-		_putchar(a);
+			continue;
 		}
-		_putchar('\n');
+		_putchar(a);
 	}
+		_putchar('\n');
 }
