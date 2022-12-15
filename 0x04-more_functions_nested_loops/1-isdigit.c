7a47152595c86ed2f696732d45eeb36e37716e26
maryamawitG <maryamawit4kidus@gmail.com>
2022-12-15 10:37:59 -0800

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a difit
+ * @c: the digit
+ * Return: 1 if the number is digit , 0 if otherwise
+ */
+
+#include "main.h"
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
