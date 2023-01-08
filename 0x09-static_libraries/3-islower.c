6b77bbf5142e418ac12b0f835fe8b5fed99404e1
maryamawitG <maryamawit4kidus@gmail.com>
2023-01-08 01:00:59 -0800

    et

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -6,4 +6,5 @@
  */
 int _islower(int c)
 {
+return (c >= 'a' && c <= 'z');
 }
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -7,4 +7,6 @@
  */
 void _puts(char *str)
 {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
