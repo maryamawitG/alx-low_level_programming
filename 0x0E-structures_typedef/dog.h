e5cdb667cc8cd48befe75c0608d04d2dac32d142
maryamawitG <maryamawit4kidus@gmail.com>
2023-01-17 03:42:00 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @ownner: owner
+ * Description: this struct is for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
